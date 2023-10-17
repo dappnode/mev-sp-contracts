71f1a924c1e659c372d74fe361b290372b58a833
invocamanman <jesus.ligero@yahoo.es>
2023-10-17 20:36:05 +0200

    update solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,12 +17,13 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.21",
         settings: {
           optimizer: {
             enabled: true,
             runs: 999999
-          }
+          },
+          evmVersion: "shanghai",
         }
       },
       {
