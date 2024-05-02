4d15227de27ab49a26c748756c4d963a20d83df0
invocamanman <jesus.ligero@yahoo.es>
2024-05-02 13:42:52 +0200

    add holesky support

--- hardhat.config.js
+++ hardhat.config.js
@@ -63,8 +63,8 @@ module.exports = {
         count: 20,
       },
     },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    holesky: {
+      url: `https://holesky.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
         path: "m/44'/60'/0'/0",
@@ -106,7 +106,22 @@ module.exports = {
     noColors: !!process.env.REPORT_GAS_FILE,
   },
   etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`
-  }
+    apiKey: {
+        holesky: `${process.env.ETHERSCAN_API_KEY}`,
+        sepolia: `${process.env.ETHERSCAN_API_KEY}`,
+        mainnet: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+    customChains: [
+        {
+            network: "holesky",
+            chainId: 17000,
+            urls: {
+                apiURL: "https://api-holesky.etherscan.io/api",
+                browserURL: "https://holesky.etherscan.io/",
+            },
+        },
+    ],
+},
+
 };
 
