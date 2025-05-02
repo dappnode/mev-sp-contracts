bb8a7139e126c02ca7c6e8e4045b4acf9716c9d1
Marketen <marcfont12@gmail.com>
2025-05-02 13:54:38 +0200

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
 require("hardhat-dependency-compiler");
 
-DEFAULT_MNEMONIC = "test test test test test test test test test test test junk";
+DEFAULT_MNEMONIC =
+  "test test test test test test test test test test test junk";
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -13,11 +14,9 @@ DEFAULT_MNEMONIC = "test test test test test test test test test test test junk"
  */
 module.exports = {
   dependencyCompiler: {
-    paths: [
-        "@openzeppelin/contracts/governance/TimelockController.sol",
-    ], // ,
+    paths: ["@openzeppelin/contracts/governance/TimelockController.sol"], // ,
     // keep: true
-},
+  },
   solidity: {
     compilers: [
       {
@@ -25,100 +24,51 @@ module.exports = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
+            runs: 999999,
           },
           evmVersion: "shanghai",
-        }
+        },
       },
       {
         version: "0.6.11",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
-      }
-    ]
+            runs: 999999,
+          },
+        },
+      },
+    ],
   },
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    holesky: {
-      url: `https://holesky.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
-    hardhat: {
-      initialDate: "0",
-      accounts: {
-        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
+    kurtosis: {
+      url: process.env.KURTOSIS_RPC_URL || "http://65.109.102.216:42002/", // adjust as needed
+      chainId: 3151908,
+      accounts: process.env.PVTK_DEPLOYMENT
+        ? [process.env.PVTK_DEPLOYMENT]
+        : [],
     },
   },
   gasReporter: {
     enabled: !!process.env.REPORT_GAS,
-    outputFile: process.env.REPORT_GAS_FILE ? './gas_report.md' : null,
+    outputFile: process.env.REPORT_GAS_FILE ? "./gas_report.md" : null,
     noColors: !!process.env.REPORT_GAS_FILE,
   },
   etherscan: {
     apiKey: {
-        holesky: `${process.env.ETHERSCAN_API_KEY}`,
-        sepolia: `${process.env.ETHERSCAN_API_KEY}`,
-        mainnet: `${process.env.ETHERSCAN_API_KEY}`,
+      hoodi: `${process.env.ETHERSCAN_API_KEY}`,
+      sepolia: `${process.env.ETHERSCAN_API_KEY}`,
+      mainnet: `${process.env.ETHERSCAN_API_KEY}`,
     },
     customChains: [
-        {
-            network: "holesky",
-            chainId: 17000,
-            urls: {
-                apiURL: "https://api-holesky.etherscan.io/api",
-                browserURL: "https://holesky.etherscan.io/",
-            },
+      {
+        network: "kurtosis",
+        chainId: 3151908,
+        urls: {
+          apiURL: "https://api-hoodi.etherscan.io/api",
+          browserURL: "https://hoodi.etherscan.io/",
         },
+      },
     ],
-},
-
+  },
 };
-
